80bcf75ba935ef2749788a56e6f4d8ae06486e2c
yesi fita <yesifita@gmail.com>
2023-05-25 21:15:38 -0300

    add link a profile add name session

--- src/routes/recDashboard.tsx
+++ src/routes/recDashboard.tsx
@@ -4,24 +4,25 @@ import CardNewJob from "../componentes/Apliccants/CardNewJob";
 import NavMenu from "../componentes/navMenu";
 import CardNewPostulates from "../componentes/Apliccants/CardNewPostulates";
 import CardNewTotals from "../componentes/Apliccants/CardNewTotals";
-
+import { useUser } from "../context/UserProvider";
 
 
 export default function RecDashboard() {
+    const authUser=useUser();
+    const {displayName}= authUser.user;
     return (
-        <div className="flex relative">
+        <div className="relative flex">
 
 
             <div className="z-10">
                 <NavMenu/>
             </div>
 
+            <div className="absolute px-6 left-20">
+                <nav className="flex items-center justify-between">
 
-
-
-            <div className=" absolute left-20 px-6">
-                <nav>
-                    <Link to={'/'} className="block text-2xl font-bold p-4">Reclutando</Link>
+                    <Link to={'/'} className="block p-4 text-2xl font-bold">Reclutando</Link>
+                    {displayName && <h1 className="text-2xl font-bold text-black pl-28">Hola {displayName}!</h1>}
                     <hr />
                 </nav>
 
@@ -31,8 +32,8 @@ export default function RecDashboard() {
 
 
 
-                <div className="flex flex-col items-center mx-auto p-4">
-                    <div className="flex flex-wrap my-8 w-full">
+                <div className="flex flex-col items-center p-4 mx-auto">
+                    <div className="flex flex-wrap w-full my-8">
                         <div className="w-1/2 pr-4">
                             <CardWelcome/>    
                         </div>
